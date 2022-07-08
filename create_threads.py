ad6e7517820ebb0cfa8cdb53ecd2faf0b83ad34c
Damon <dmarcr1997@gmail.com>
2022-07-08 12:56:58 -0400

    Created thread file and connection to ssh file

---
+++ create_threads.py
@@ -0,0 +1,13 @@
+import threading
+
+def create_threads(list, function):
+
+    threads = []
+
+    for ip in list:
+        th = threading.Thread(target=function, args=(ip,))
+        th.start()
+        threads.append(th)
+    
+    for th in threads:
+        th.join()
